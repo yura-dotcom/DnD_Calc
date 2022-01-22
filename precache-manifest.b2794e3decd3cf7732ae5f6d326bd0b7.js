eb2fd2a4007bca320f83c786c6352e5bc83a45c7
master <devgeorg222@gmail.com>
2022-01-22 16:08:04 +0200

    Updates

---
+++ precache-manifest.b2794e3decd3cf7732ae5f6d326bd0b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54177fdb80cfd14ae8e2653401cf38fe",
+    "url": "/DnD_Calc/index.html"
+  },
+  {
+    "revision": "4f27642c76e3ef5c6e15",
+    "url": "/DnD_Calc/static/css/main.21fa855e.chunk.css"
+  },
+  {
+    "revision": "ff3a11ba4fe91eea35cd",
+    "url": "/DnD_Calc/static/js/2.52783562.chunk.js"
+  },
+  {
+    "revision": "52fc661712429cc9af7b9bed5de064e2",
+    "url": "/DnD_Calc/static/js/2.52783562.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f27642c76e3ef5c6e15",
+    "url": "/DnD_Calc/static/js/main.15e79bc2.chunk.js"
+  },
+  {
+    "revision": "fa4aaa0814aabdd3b660",
+    "url": "/DnD_Calc/static/js/runtime-main.a8866aa4.js"
+  }
+]);
